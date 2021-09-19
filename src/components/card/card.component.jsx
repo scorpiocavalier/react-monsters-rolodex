fcec49ffe2e0031ddf59a5933dc957625114199a
Arturo Chen <art.can.fly@gmail.com>
2021-09-19 02:19:09 -0400

    + CardList and Card Components

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = ({ monster }) => {
+  const { id, name, email } = monster;
+
+  return (
+    <div className='card-container'>
+      <img
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        alt='monster'
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
